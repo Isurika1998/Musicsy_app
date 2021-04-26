458d67b802262e4574d2d55705cdcdc0f31e0a89
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-04-26 23:43:05 +0530

    progress bar and songs

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private File directory;
+
+    @FXML
+    private File[] files;
+
+    @FXML
+    private ArrayList<File> songs;
+
+    @FXML
+    private Button playBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        songs = new ArrayList<File>();
+        directory = new File("music");
+        files = directory.listFiles();
+        if (files != null){
+            for (File file : files) {
+                songs.add(file);
+            }
+        }
+    }
+
+    public void playMedia(){
+
+    }
 }
