f3d1acaa5e05864be0517c5f671d6da6328a4c07
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-04-27 01:13:37 +0530

    play,next,prev button functions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import java.io.File;
 import java.net.URL;
@@ -21,7 +24,16 @@ public class Controller implements Initializable {
     private ArrayList<File> songs;
 
     @FXML
-    private Button playBtn;
+    private Button playBtn, nextBtn, prevBtn;
+
+    @FXML
+    private Label songLbl;
+
+    private int songNumber;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private int status;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,9 +45,50 @@ public class Controller implements Initializable {
                 songs.add(file);
             }
         }
+
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        songLbl.setText(songs.get(songNumber).getName());
     }
 
-    public void playMedia(){
 
+    public void playMedia(){
+        if (status == 0){
+            mediaPlayer.play();
+            status=1;
+        }else if(status == 1){
+            mediaPlayer.pause();
+            status=0;
+        }
+
+    }
+
+    public void nextMedia(){
+        if(songNumber < songs.size()-1){
+            songNumber++;
+        }else {
+            songNumber = 0;
+        }
+        mediaPlayer.stop();
+        status=0;
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLbl.setText(songs.get(songNumber).getName());
+        playMedia();
+    }
+
+    public void prevMedia(){
+        if(songNumber > 0){
+            songNumber--;
+        }else {
+            songNumber = songs.size() - 1;
+        }
+        mediaPlayer.stop();
+        status=0;
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLbl.setText(songs.get(songNumber).getName());
+        playMedia();
     }
 }
