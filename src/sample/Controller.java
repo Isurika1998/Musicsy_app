8db39835d562fdfd4dcf8ef97ff0538d6b9699bd
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-04-29 21:08:07 +0530

    song table

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,13 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -11,6 +15,8 @@ import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Controller implements Initializable {
 
@@ -24,26 +30,47 @@ public class Controller implements Initializable {
     private ArrayList<File> songs;
 
     @FXML
-    private Button playBtn, nextBtn, prevBtn;
+    private Button playBtn, nextBtn, prevBtn, nameBtn;
 
     @FXML
     private Label songLbl;
+
+    @FXML
+    private HBox songName;
+
+    @FXML
+    private Pane songPane;
+
+    @FXML
+    private TableView songTbl;
 
     private int songNumber;
 
     private Media media;
     private MediaPlayer mediaPlayer;
     private int status;
+    private ObservableList<ObservableList> data;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         songs = new ArrayList<File>();
         directory = new File("music");
         files = directory.listFiles();
+        data = FXCollections.observableArrayList();
+
+        ObservableList<String> row = FXCollections.observableArrayList();
         if (files != null){
             for (File file : files) {
                 songs.add(file);
+                row.add(file.getName());
+                HBox hb = new HBox();
+                JFXButton b = new JFXButton(file.getName());
+                hb.getChildren().add(b);
+                songPane.getChildren().add(hb);
+
             }
+            data.add(row);
+            songTbl.getItems().add(data);
         }
 
         media = new Media(songs.get(songNumber).toURI().toString());
