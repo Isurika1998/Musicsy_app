167f0006b199c7cf767dc3a5a144b736a6f32649
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-05-10 23:34:00 +0530

    icon color

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -302,9 +302,9 @@ public class Controller implements Initializable {
 
             // create the mysql insert preparedstatement
             PreparedStatement preparedStmt = con.prepareStatement(query);
-            preparedStmt.setInt(1, 6);
+            preparedStmt.setInt(1, 7);
             preparedStmt.setString (2, songs.get(songNumber).toURI().toString());
-            favIcon.setStyle("-fx-fill-color : red");
+            favIcon.setStyle("color : red");
 
             // execute the preparedstatement
             preparedStmt.execute();
